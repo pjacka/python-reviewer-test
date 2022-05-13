ff087e5a5b3c75e0b95e9541945c8147642476a9
pjacka <petr.jacka@cern.ch>
2022-05-13 12:34:10 +0200

    Fix lab2 demo code

--- lab-2.py
+++ lab-2.py
@@ -1,28 +1,35 @@
 
-operations_count = 0
+
 
 def main():
+    operations_count = 0
     ask_again = True
     while(ask_again):
         a = input("Enter the numerator: ")
         b = input("Enter the denominator: ")
         result = perform_division(a,b)
-        print(result)
+        if result is not None:
+            operations_count+= 1
+            print(result)
+        
         ask_again = input("Do you want to perform another operation? Enter yes or no: ")
         if(ask_again == 'yes'):
             ask_again = True
         else:
             ask_again = False
-            print("You performed " + str(operations_count) + " operations, bye!")
+            
+
+
 
 
 def perform_division(a,b):
     global operations_count
     try:
-        operations_count += 1
         return int(a)/int(b)
+    except ZeroDivisionError as error:
+        print("Looks like you are trying to divide by zero! This is not possible.")        
     except Exception as e:
-        pass
+        print("Invalid numbers")
 
 
 main()
